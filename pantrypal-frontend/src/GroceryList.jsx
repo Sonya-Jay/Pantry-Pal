f2c440955b9375163c41551a147d2b0b78195b52
Sonya-Jay <sonya.jayathilake@gmail.com>
2025-04-15 13:51:24 -0500

    some color fixes

--- pantrypal-frontend/src/GroceryList.jsx
+++ pantrypal-frontend/src/GroceryList.jsx
@@ -44,11 +44,47 @@ const GroceryList = ({ groceries, onDelete }) => {
           }}
           className="fixed inset-0 flex justify-center items-center z-50"
         >
+          <div
+            className="relative max-w-md w-full p-6 rounded shadow-lg"
+            style={{ backgroundColor: "#78b889" }}
+          >
+            <button
+              onClick={() => setNutrition(null)}
+              style={{ backgroundColor: "transparent", boxShadow: "none" }}
+              className="absolute top-3 right-3 text-gray-600 hover:text-red-500 p-1 rounded-full focus:outline-none"
+              aria-label="Close"
+            >
+              <X
+                size={20}
+                style={{ backgroundColor: "transparent" }}
+                color="#0b5511"
+              />
+            </button>
+            <h2 className="text-xl font-semibold mb-4">
+              Nutrition Facts for {nutrition.name}
+            </h2>
+            <ul className="list-disc list-inside text-left text-sm">
+              <li>Calories: {nutrition.calories || "N/A"}</li>
+              <li>Protein: {nutrition.protein || "N/A"}</li>
+              <li>Fat: {nutrition.fat || "N/A"}</li>
+              <li>Carbs: {nutrition.carbs || "N/A"}</li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {/* {nutrition && (
+        <div
+          style={{
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            backdropFilter: "blur(5px)",
+          }}
+          className="fixed inset-0 flex justify-center items-center z-50"
+        >
           <div className="relative bg-white text-black p-6 rounded shadow-lg max-w-md w-full">
             <button
               onClick={() => setNutrition(null)}
               style={{ backgroundColor: "transparent", boxShadow: "none" }}
-              className="absolute top-2 right-2 text-gray-600 hover:text-red-500 p-1 rounded-full focus:outline-none"
+              className="absolute top-2 right-2 text-[#0b5511] hover:text-red-500 p-1 rounded-full focus:outline-none"
               aria-label="Close"
             >
               <X size={20} style={{ backgroundColor: "transparent" }} />
@@ -64,7 +100,7 @@ const GroceryList = ({ groceries, onDelete }) => {
             </ul>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
